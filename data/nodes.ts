09ec131e7d6783d07383dc3cd46d9dc02d6eb885
Yash Sharma <44340561+yash-sharma1@users.noreply.github.com>
2022-04-22 20:59:20 +0530

    Added DataHub

--- data/nodes.ts
+++ data/nodes.ts
@@ -154,6 +154,14 @@ const nodes: Node[] = [
     status: true,
     secret: true,
   },
+  {
+    name: 'DataHub',
+    price: 'Freemium',
+    endpoint: `https://ethereum-mainnet--rpc.datahub.figment.io/apikey/${process.env.DATAHUB_API_KEY}`,
+    website: 'https://datahub.figment.io/',
+    status: true,
+    secret: true,
+  },
 ];
 
 function finish(start: number, endpoint: string) {
